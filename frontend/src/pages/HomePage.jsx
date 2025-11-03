392b686da35aba23e8fe4024eac1fb3b329980c2
munnaMia <mdmunnamia987@gmail.com>
2025-11-04 01:49:50 +0600

    update: Header JSX by importing css

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import Header from "../components/Header";
+import "./HomePage.css";
 
 export default function HomePage() {
     return (
-        <>
+        <div className="page">
             <Header />
             <h1>This gonna be the home page</h1>
-        </>
+        </div>
     );
 }
