8d39ae5e89597cfa5755e1ba61e5e811f085ae7b
munnaMia <mdmunnamia987@gmail.com>
2025-11-04 01:47:35 +0600

    update: Header jsx & add css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,10 @@
 import HeaderTop from "./HeaderTop";
 import HeaderBottom from "./HeaderBottom";
-
+import './Header.css'
 export default function Header() {
     return (
         <header className="headerSection">
             <HeaderTop />
-
             <HeaderBottom />
         </header>
     );
