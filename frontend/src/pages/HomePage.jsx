edc8b38497aabcdab55542420d6dd4507e123c82
munnaMia <mdmunnamia987@gmail.com>
2025-11-12 23:49:29 +0600

    add: Motivaiton componenst with slight style changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import Motivation from "../components/common/Motivation";
 import Header from "../components/layout/header/Header";
 import HomeDashboard from "../components/layout/home_dashboard/HomeDashboard";
 import "./HomePage.css";
@@ -7,8 +8,10 @@ export default function HomePage() {
         <div className="page">
             <Header />
 
+            <Motivation />
+
             <HomeDashboard />
-            
+
             <h1>This gonna be the home page</h1>
         </div>
     );
